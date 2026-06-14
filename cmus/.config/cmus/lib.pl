cda0cdf2736d44a1f17ffbe274b513cc91829260
Aaron Conway <ajconway92@gmail.com>
2026-06-14 01:38:19 -0500

    add mpv, cmus, wireplumber to dotfiles

---
+++ cmus/.config/cmus/lib.pl
@@ -0,0 +1,68 @@
+/home/aaron/Music/Don Toliver Love Sick 2023/LoveSickness.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Let Her Go (feat. James Blake).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Leave The Club (feat. Lil Durk & GloRilla).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/4 Me (feat. Kali Uchis).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Go Down (feat. TisaKorean).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Time Heals All.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Leather Coat.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Honeymoon.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Private Landing (feat. Justin Bieber & Future).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Slow Motion (feat. Wizkid).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Do It Right.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/If I Had (feat. Charlie Wilson).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Company Pt. 3.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Bus Stop (feat. Brent Faiyaz).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Cinderella (feat. Toro y Moi).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/Encouragement.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/KRYPTONITE.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/TORE UP.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/BROTHER STONE (FEAT. KODAK BLACK).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/ATTITUDE (FEAT. CHARLIE WILSON & CASH COBAIN).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/BANDIT.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/GLOCK.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/ICE AGE (FEAT. TRAVIS SCOTT).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/4X4.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/PURPLE RAIN (FEAT. FUTURE & METRO BOOMIN).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/NEW DROP.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/BACKSTREETS (FEAT. TEEZO TOUCHDOWN).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/DEEP IN THE WATER.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/INSIDE (FEAT. TRAVIS SCOTT).mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/5 TO 10.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/LAST LAUGH.mp3
+/home/aaron/Music/Don Toliver Love Sick 2023/HARDSTONE NATIONAL ANTHEM.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Tiramisu.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/E85.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Body.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Rendezvous.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Secondhand.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/ATM.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Long Way To Calabasas.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Rosary.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/All The Signs.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Call Back.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Tuition.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/K9.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Excavator.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Gemstone.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/OPPOSITE.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/TMU.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Pleasure_s Mine.mp3
+/home/aaron/Music/Don Toliver OCTANE 2026/Sweet Home.mp3
+/home/aaron/Music/Drake - Iceman 2026/Make Them Cry.mp3
+/home/aaron/Music/Drake - Iceman 2026/Dust.mp3
+/home/aaron/Music/Drake - Iceman 2026/Whisper My Name.mp3
+/home/aaron/Music/Drake - Iceman 2026/Janice STFU.mp3
+/home/aaron/Music/Drake - Iceman 2026/Ran to Atlanta feat. Future & Molly Santana.mp3
+/home/aaron/Music/Drake - Iceman 2026/Shabang.mp3
+/home/aaron/Music/Drake - Iceman 2026/Make Them Pay.mp3
+/home/aaron/Music/Drake - Iceman 2026/Burning Bridges.mp3
+/home/aaron/Music/Drake - Iceman 2026/National Treasures.mp3
+/home/aaron/Music/Drake - Iceman 2026/B's on the Table feat. 21 Savage.mp3
+/home/aaron/Music/Drake - Iceman 2026/What Did I Miss_.mp3
+/home/aaron/Music/Drake - Iceman 2026/Plot Twist.mp3
+/home/aaron/Music/Drake - Iceman 2026/2 Hard 4 the Radio.mp3
+/home/aaron/Music/Drake - Iceman 2026/Make Them Remember.mp3
+/home/aaron/Music/Drake - Iceman 2026/Little Birdie.mp3
+/home/aaron/Music/Drake - Iceman 2026/Don't Worry.mp3
+/home/aaron/Music/Drake - Iceman 2026/Firm Friends.mp3
+/home/aaron/Music/Drake - Iceman 2026/Make Them Know.mp3
